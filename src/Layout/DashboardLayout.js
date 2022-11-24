ab3b3edee97e8e337607a9b705174d1c8415a55b
Taslim <taslimahmed313@gmil.com>
2022-11-25 02:47:14 +0600

    Load Dashboard Buyer and Conditional Render

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../Contexts/AuthProvider';
+import useBuyer from '../hooks/useBuyer';
 import useSeller from '../hooks/useSeller';
 import Navbar from '../Pages/Navbar/Navbar';
 
 const DashboardLayout = () => {
     const {user} = useContext(AuthContext);
-    const [isSeller] = useSeller(user?.email)    
+    const [isSeller] = useSeller(user?.email);
+    const [isBuyer] = useBuyer(user?.email);    
     
     return (
       <div>
@@ -26,11 +28,13 @@ const DashboardLayout = () => {
               className="drawer-overlay"
             ></label>
             <ul className="menu p-4 w-80 text-base-content">
-              <>
-                <li>
-                  <Link to="/dashboard">My Orders</Link>
-                </li>
-              </>
+              {isBuyer && (
+                <>
+                  <li>
+                    <Link>My Orders</Link>
+                  </li>
+                </>
+              )}
               {isSeller && (
                 <>
                   <li>
---
+++ src/Routers/BuyerRouter/BuyerRouter.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { DotLoader } from "react-spinners";
+import { AuthContext } from '../../Contexts/AuthProvider';
+import useBuyer from '../../hooks/useBuyer';
+
+const BuyerRouter = ({children}) => {
+    const { user, loading } = useContext(AuthContext);
+    const [isBuyer, isBuyerLoading] = useBuyer()
+    const location = useLocation();
+
+    if (loading || isBuyerLoading) {
+      return <DotLoader color="#36d7b7"></DotLoader>;
+    }
+    if (user && isBuyer) {
+      return children;
+    }
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default BuyerRouter;

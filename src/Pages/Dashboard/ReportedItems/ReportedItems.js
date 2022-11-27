a1094bd37faa80b99bf7dc696d758b9b883a90ea
Taslim <taslimahmed313@gmil.com>
2022-11-27 10:39:36 +0600

    Reported Product Remove Successfully

--- src/Pages/Dashboard/ReportedItems/ReportedItems.js
+++ src/Pages/Dashboard/ReportedItems/ReportedItems.js
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const ReportedItems = () => {
 
-    const {data: reports = []} = useQuery({
+    const {data: reports = [], refetch} = useQuery({
         queryKey: ["reports"],
         queryFn: async() =>{
             const res = await fetch("http://localhost:5000/reports");
@@ -11,12 +12,25 @@ const ReportedItems = () => {
             return data;
         }
     })
-    console.log(reports)
+
+    const handleReportedProductDelete = reported =>{
+      fetch(`http://localhost:5000/reports/${reported._id}`, {
+        method: "DELETE"
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if(data.deletedCount > 0){
+          toast.success(`Reported Book ${reported.name} Remove From The BookList`);
+          refetch();
+        }
+      })
+    }
 
     return (
       <div>
         <h1 className="text-3xl font-serif font-semibold text-center my-4">
-          All Sellers Data Here
+          All Reported Items Here
         </h1>
         <div className="overflow-x-auto">
           <table className="table w-full">
@@ -48,7 +62,10 @@ const ReportedItems = () => {
                   </td>
 
                   <td>
-                    <button className=" text-white font-serif capitalize border-0 btn-sm bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500">
+                    <button
+                      onClick={() => handleReportedProductDelete(report)}
+                      className="btn text-white font-serif capitalize border-0 btn-sm bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500"
+                    >
                       Delete
                     </button>
                   </td>
